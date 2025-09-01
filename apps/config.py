b75ac559e97381c8e1360943863c437b88f5a94a
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-09-01 20:13:28 -0300

    feat configuração para o aplicativo flask

--- apps/config.py
+++ apps/config.py
@@ -3,3 +3,22 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# 2 - Estou criando uma instância do Flask e salvando na variável app
+# Flask é a classe "molde" ou a "planta" para cria um aplicativo web
+
+app = Flask(__name__)
+
+
+# 3 - Configuração para o aplicativo flask 
+
+# estou definindo o endereço IP que o servidor irá "escutar"
+# No caso o "0.0.0.0" significa que ele está disponível para qualquer endereço IP,
+# como container e embiente virtual.
+# A porta onde o app Flask irá rodar, 5000 é do flask e 5002 defini agora
+# Quando o DEBUG é True o Flask ativa as seguintes funcionalidades:
+# O Flask reinicia o servidor automaticamente toda vez que faz alteração no seu código fonte
+# e caso haja um erro na aplicação o Flask exibirá um depurador interativo no navegador
+
+app.config["HOST"] = "0.0.0.0" 
+app.config["PORT"] = 5002 
+app.config["DEBUG"] = True # 
