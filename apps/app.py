8016303a5f0486231da4bc83e106045e3ec70d3d
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-09-07 15:45:20 -0300

    Update: corrigindo register_blueprint

--- apps/app.py
+++ apps/app.py
@@ -2,7 +2,7 @@ from config import app, db_serv
 from empresa.route_empresa import bd_Empresa
 from flask_sqlalchemy import SQLAlchemy
 
-app.register_blueprinte(bd_Empresa)
+app.register_blueprint(bd_Empresa)
 
 if __name__ == '__main__':
     app.run(host = 'localhost', port= 5002, debug= True)
