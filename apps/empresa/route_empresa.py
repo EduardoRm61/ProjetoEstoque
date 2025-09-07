5acb57ef86ac4e9b9c23b1bda2eb793280c55ec9
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-09-07 12:57:17 -0300

    Update: corrigindo a rota GET

--- apps/empresa/route_empresa.py
+++ apps/empresa/route_empresa.py
@@ -1,10 +1,16 @@
 from flask import Blueprint, request, jsonify
 import empresa.model_empresa as modEmp
 
-bd_Empresa = Blueprint("/empresa", methods=["GET"])
+
+
+bd_Empresa = Blueprint("/empresa", __name__)
+
+
+@bd_Empresa.route("/empresa", methods=["GET"])
 def listar_empresas():
     try:
         empresa = modEmp.listarEmpresa()
         return jsonify (empresa)
     except Exception as e:
         return {"Erro": str(e)}, 400
+    
